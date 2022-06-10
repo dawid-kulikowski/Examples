5d3a53870192d34c5547827a60aaa23cdf6e294a
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-06-10 15:09:42 +0200

    Examples - Loops

---
+++ src/main/java/Examples.java
@@ -0,0 +1,10 @@
+public class Examples {
+    public static void main(String[] args) {
+        System.out.println("Examples of loops");
+        Loops loops = new Loops(5,5,6,"/");
+        loops.nestedLoop();
+        loops.whileLoop();
+        loops.doWhileLoop();
+        loops.forLoop();
+    }
+}
---
+++ src/main/java/Loops.java
@@ -0,0 +1,50 @@
+public class Loops {
+    protected int a;
+    protected int b;
+    protected int c;
+    protected String symbol;
+
+    // Constructor
+    public Loops(int a, int b, int c, String symbol) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.symbol = symbol;
+    }
+
+    // whileLoop - executes as long as condition is true
+    public void whileLoop(){
+    while (a<100) {
+        a++;
+        System.out.println(a);
+        }
+    }
+
+    // doWhileLoop - executes as long as condition is true (will execute once no matter what conditions are)
+    public void doWhileLoop(){
+    do {
+        b++;
+        System.out.println(b);
+    } while (b<100);
+    }
+
+
+// for loop - loop that executes block of code a limited amount of times
+   public void forLoop(){
+        for(int i=0; i<=b; i+=10){
+            System.out.println(i);
+        }
+    }
+
+// nestedLooop - loop inside  of a loop
+   public void nestedLoop(){
+        for(int i=1; i<a; i++){
+            System.out.println("");
+            for(int j=1; j<b; j++) {
+                System.out.print("|");
+            }
+        }
+    }
+
+
+}
