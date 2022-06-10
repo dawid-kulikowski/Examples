ea47177775a6620c308e6cd715d6bfe91189f0a9
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-06-10 20:02:56 +0200

    Examples - 2D Array

--- src/main/java/Arrays.java
+++ src/main/java/Arrays.java
@@ -2,9 +2,11 @@ public class Arrays {
 
     // array - store multiple values in a single variable
     String[] car;
+    String[][] cars;
 
-    public Arrays(String[] car) {
+    public Arrays(String[] car, String[][] cars) {
         this.car = car;
+        this.cars = cars;
     }
     public void simpleArray() {
         car[0] = "Fiat";
@@ -14,6 +16,26 @@ public class Arrays {
         for(int i=0; i<car.length; i++) {
             System.out.println(car[i]);
         }
+    }
 
+    // 2d array - array of arrays
+
+    public void multiDimensionArray(){
+        cars[0][0] = "Audi";
+        cars[0][1] = "Ford";
+        cars[0][2] = "Fiat";
+        cars[1][0] = "Volkswagen";
+        cars[1][1] = "Renault";
+        cars[1][2] = "Peugeot";
+        cars[2][0] = "Opel";
+        cars[2][1] = "Alfa Romeo";
+        cars[2][2] = "BMW";
+
+        for (int i=0;i<cars.length;i++) {
+            System.out.println();
+            for(int j=0;j<cars[i].length; j++) {
+                System.out.println(cars[i][j]+" ");
+            }
+        }
     }
 }
--- src/main/java/Examples.java
+++ src/main/java/Examples.java
@@ -1,14 +1,15 @@
 public class Examples {
     public static void main(String[] args) {
-        System.out.println("Examples of loops");
-        Loops loops = new Loops(5,5,6,"/");
-//        loops.nestedLoop();
-//        loops.whileLoop();
-//        loops.doWhileLoop();
-//        loops.forLoop();
+//        System.out.println("Examples of loops");
+//        Loops loops = new Loops(5,5,6,"/");
+////        loops.nestedLoop();
+////        loops.whileLoop();
+////        loops.doWhileLoop();
+////        loops.forLoop();
 
-        Arrays arrays = new Arrays(new String[3]);
+        Arrays arrays = new Arrays(new String[3],new String[3][3]);
         arrays.simpleArray();
+        arrays.multiDimensionArray();
 
     }
 }
